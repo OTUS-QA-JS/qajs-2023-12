278d6c8a85d77318b35a5638a64860118ac77df6
Damir Rysaev <freepad@ya.ru>
2024-01-30 16:39:35 +0300

    got example

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -1,18 +1,22 @@
 import { config } from '../framework'
-import supertest from 'supertest'
+import got from 'got'
+
+/**
+ * Этот пример не будет работать!
+ * Пока проект не переедет на es modules (настоящие)
+ */
 
 describe('Авторизация', () => {
   it('Успешная авторизация', async () => {
-    const response = await supertest(config.baseURL)
-      .post('/Account/v1/GenerateToken')
-      .send({
-        userName: config.username,
-        password: config.password,
-      })
-    expect(response.status).toBe(200)
+    const url = `${config.baseURL}/Account/v1/GenerateToken`
+
+    const response = await got.post(url, {
+      json: { userName: config.username, password: config.password },
+      responseType: 'json',
+    })
+
+    expect(response.statusCode).toBe(200)
     expect(response.body.result).toBe('User authorized successfully.')
     expect(response.body.token).toBeDefined()
   })
 })
-
-
