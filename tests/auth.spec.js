a40a0c07767b7997ef57c879e3a1421b0e5c645d
Damir Rysaev <freepad@ya.ru>
2024-01-30 15:55:26 +0300

    fetch example

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -1,19 +1,20 @@
-import axios from 'axios'
 import { config } from '../framework'
-
-const client = axios.create({
-  baseURL: config.baseURL,
-  validateStatus: () => true,
-})
 
 describe('Авторизация', () => {
   it('Успешная авторизация', async () => {
-    const response = await client.post('/Account/v1/GenerateToken', {
-      userName: config.username,
-      password: config.password,
+    const url = `${config.baseURL}/Account/v1/GenerateToken`
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        userName: config.username,
+        password: config.password,
+      }),
     })
     expect(response.status).toBe(200)
-    expect(response.data.result).toBe('User authorized successfully.')
-    expect(response.data.token).toBeDefined()
+
+    const data = await response.json()
+    expect(data.result).toBe('User authorized successfully.')
+    expect(data.token).toBeDefined()
   })
 })
